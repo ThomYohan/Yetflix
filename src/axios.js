7523b974c959cdd9f8a03f198f47b983f415de16
Thomas Berhe Ghebreyohannes <Woolu@thomass-mbp.attlocal.net>
2020-07-20 17:50:58 -0500

    data fetch success/now working on finishing data fetch for all rows

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// its a default import, thats why we can use the name axios
+
 /** base url to make requests to the movie database */
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
--- src/row.js
+++ src/row.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row({title}) {
+function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
+
+    // a snippet of code which runs based on a specific condition
+    useEffect(() => {
+        // if [] is blank, run once when the row loads and dont run again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request)
+            return request
+        }
+        fetchData()
+    }, []);
 
     return (
         <div>

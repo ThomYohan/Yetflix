f3addd7fddd83074ff298aedb2ad4ec0b0717e8f
Thomas Berhe Ghebreyohannes <Woolu@thomass-mbp.attlocal.net>
2020-07-20 17:34:18 -0500

    starting to fetch data/building out rows

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>weewoo</h1>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
-      <Row title="Top Rated"/>
-      <Row title="Action Movies"/>
-      <Row title="Comedy Movies"/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now"fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated"fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies"fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies"fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies Movies"fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies"fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries"fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }

34df27dc954bb1406e6b5fa177f86922d658009b
Thomas Berhe Ghebreyohannes <thomasgyohannes@gmail.com>
2020-07-21 16:08:06 -0500

    styling tweaks on rows

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>weewoo</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now"fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated"fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies"fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies"fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Horror Movies Movies"fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romance Movies"fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title="Documentaries"fetchUrl={requests.fetchDocumentaries}/>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow="true"
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/row.js
+++ src/row.js
@@ -1,39 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
-import './Row.css'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    // a snippet of code which runs based on a specific condition
-    useEffect(() => {
-        // if [] is blank, run once when the row loads and dont run again
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData()
-        // console.table(movies)
-    }, [fetchUrl]);
-    
-    return (
-        <div className="row">
-            <h2>{title}</h2>
+  // a snippet of code which runs based on a specific condition
+  useEffect(() => {
+    // if [] is blank, run once when the row loads and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+    // console.table(movies)
+  }, [fetchUrl]);
 
-            <div className="row_posters">
-            {movies.map( movie => (
-                <img 
-                className="row_poster"
-                src={`${base_url}${movie.poster_path}`} 
-                alt={movie.name}/>
-            ))}
-            </div>
-            {/* container -> posters */}
-        </div>
-    )
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map(movie => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* container -> posters */}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
